1b53fca944ad1a9b9f3327bc6159bb0a8424bfc3
Daniel Kiss <kissd621@gmail.com>
2020-07-28 18:29:08 +0200

    Added google analytics

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import VueProgressBar from 'vue-progressbar';
+import VueAnalytics from 'vue-analytics';
 
 Vue.config.productionTip = false;
+
+Vue.use(VueAnalytics, { id: 'UA-159367806-1', router });
 
 Vue.use(VueProgressBar, {
   color: '#24b9ff',
